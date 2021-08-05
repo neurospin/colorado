dbaa41bd03ec628e713f6cf5e45fc4910b3b4a6e
Marco Pascucci <marpas.paris@gmail.com>
2021-08-05 14:12:24 +0200

    the meshing functions use a lower level function of aims

--- colorado/aims_tools.py
+++ colorado/aims_tools.py
@@ -75,11 +75,12 @@ def volume_to_ndarray(volume):
     try:
         # aims VOlume and numpy array have shape
         if len(volume.shape) > 3:
-            volume = volume[ tuple(3*[slice(0,None)] + [0]*(len(volume.shape)-3))]
+            volume = volume[tuple(3*[slice(0, None)] + [0]
+                                  * (len(volume.shape)-3))]
     except AttributeError:
         # aims.AimsData does not have shape
         # but it is always 3D
-        volume = volume[:,:,:,0]
+        volume = volume[:, :, :, 0]
     return volume[:]
 
 
@@ -90,18 +91,20 @@ def _volume_size_from_numpy_bucket(bucket_array, pad):
     # i.e. the volume always include the bucket origin.
     # This is the behaviour of AIMS
     # this also makes the volume bigger and full with zeros
-    v_max = np.maximum((0,0,0),a.max(axis=0))
-    v_min = np.minimum((0,0,0),a.min(axis=0))
+    v_max = np.maximum((0, 0, 0), a.max(axis=0))
+    v_min = np.minimum((0, 0, 0), a.min(axis=0))
     v_size = np.ceil(abs(v_max - v_min) + 1 + pad*2).astype(int)
     return v_size, v_min
+
 
 def _point_to_voxel_indices(point):
     return np.round(point).astype(int)
 
+
 def bucket_numpy_to_volume_numpy(bucket_array, pad=0, side=None):
     """Transform a bucket into a 3d boolean volume.
     Input and output types are numpy.ndarray"""
-    
+
     v_size, v_min = _volume_size_from_numpy_bucket(bucket_array, pad)
 
     vol = np.zeros(np.array(v_size))
@@ -172,15 +175,16 @@ def add_border(x, thickness, value):
 def volume_to_mesh(
         volume,
         gaussian_blur_FWWM=0,
+        threshold_absolute=None,
         threshold_quantile=0,
-        translation=(0,0,0),
-        mesh_decimation_params=dict(
+        translation=(0, 0, 0),
+        decimation_params=dict(
             reduction_rate=99,
             max_clearance=3,
             max_error=1,
             feature_angle=180),
-        mesh_smoothing_params=dict(
-            type='lowpass',
+        smoothing_params=dict(
+            algorithm='lowpass',
             iterations=30,
             rate=0.4,
             # NOT IMPLEMENTED
@@ -198,7 +202,9 @@ def volume_to_mesh(
     Args:
         volume (np.ndarray or aims.Volume): 3D binary image (if not binary, all vxels != 0 are set to 1)
         gaussian_blur_FWWM (numeric) : the gaussian blur filter's full width at half maximum.
-        threshold_quantile (float in [0,1]): voxels below this quantile will be set to zero.
+        threshold_absolute all voxels lower than this value will be set to zero.
+        threshold_quantile (float in [0,1]): voxels with intensity below this quantile will be set to zero.
+            Applied only if threshold_absolute is None.
         decimation_params (dict, optional): decimation parameters.
             reduction_rate : expected % decimation reduction rate
             max_clearance : maximum clearance of the decimation
@@ -224,8 +230,11 @@ def volume_to_mesh(
     assert(volume.max()-volume.min() != 0)
     def normalize(x): return (x-x.min())/(x.max()-x.min())
     volume = normalize(volume)
+
     # threshold
-    if threshold_quantile > 0:
+    if threshold_absolute is not None:
+        q = threshold_absolute
+    elif threshold_quantile > 0:
         q = np.quantile(volume[volume > 0], threshold_quantile)
     else:
         q = 0
@@ -240,40 +249,60 @@ def volume_to_mesh(
 
     m = aimsalgo.Mesher()
 
-    smoothing_types = {
-        "lowpass": m.LOWPASS,
-        'laplacian': m.LAPLACIAN,
-        "simplespring": m.SIMPLESPRING,
-        "polygonspring": m.POLYGONSPRING
-    }
+    # Ça renvoie un dict (label: liste de maillages, un par interface).
+    mesher_output = m.doit(aims_volume)
+    # WARGNING: the following assumes that mesher_output contains only one interface.
+    # Merge the meshes
+    meshes = list(mesher_output.values())[0]
+    merged_mesh = meshes[0]
+    for mesh in meshes[1:]:
+        aims.SurfaceManip.meshMerge(merged_mesh, mesh)
 
-    assert mesh_smoothing_params['type'].lower() in smoothing_types,\
-        "smoothing_param must be one of {}, got '{}'".format(
-            smoothing_types.keys(), mesh_smoothing_params['type'])
-
-    d = mesh_decimation_params
-    m.setDecimation(d['reduction_rate'], d['max_clearance'],
-                    d['max_error'], d['feature_angle'])
-
-    d = mesh_smoothing_params
-    m.setSmoothing(smoothing_types[d['type']], d['iterations'], d['rate'])
-
-    mesh = aims.AimsTimeSurface()
-    m.getBrain(aims_volume, mesh)
+    mesh = merged_mesh
 
     if mesh.size() == 0:
-        raise Exception("The mesh is empty, try different the parameters")
+        raise Exception("The mesh is empty, try different parameters")
 
-    mesh = PyMesh(mesh)
-    # add translation
-    mesh.vertices = mesh.vertices + translation
+    # DECIMATE
+    if decimation_params is not None:
+        d = decimation_params
+        print(d)
+        m.setDecimation(d['reduction_rate'], d['max_clearance'],
+                        d['max_error'], d['feature_angle'])
 
-    return mesh.to_aims_mesh()
+        m.decimate(mesh)
+
+    # SMOOTH
+    if smoothing_params is not None:
+        smoothing_types = {
+            "lowpass": m.LOWPASS,
+            'laplacian': m.LAPLACIAN,
+            "simplespring": m.SIMPLESPRING,
+            "polygonspring": m.POLYGONSPRING
+        }
+        assert smoothing_params['algorithm'].lower() in smoothing_types,\
+            "smoothing_param must be one of {}, got '{}'".format(
+            smoothing_types.keys(), smoothing_params['algorithm'])
+
+        d = smoothing_params
+        m.setSmoothing(
+            smoothing_types[d['algorithm']], d['iterations'], d['rate'])
+
+        m.smooth(mesh)
+
+    if translation != (0, 0, 0):
+        mesh = PyMesh(mesh)
+        # add translation
+        mesh.vertices = mesh.vertices + translation
+        mesh = mesh.to_aims_mesh()
+
+    return mesh
 
 
 def bucket_to_mesh(
         bucket,
         gaussian_blur_FWWM=0,
+        threshold_absolute=None,
         threshold_quantile=0,
         decimation_params=dict(
             reduction_rate=99,
@@ -281,7 +310,7 @@ def bucket_to_mesh(
             max_error=1,
             feature_angle=180),
         smoothing_params=dict(
-            type='lowpass',
+            algorithm='lowpass',
             iterations=30,
             rate=0.4,
             # NOT IMPLEMENTED
@@ -297,7 +326,9 @@ def bucket_to_mesh(
     Args:
         bucket (np.ndarray or aims.Volume): sequence of 3D points. 
         gaussian_blur_FWWM (numeric) : the gaussian blur filter's full with at half maximum.
-        threshold_quantile (float in [0,1]): voxels below this quantile will be set to zero.
+        threshold_absolute all voxels lower than this value will be set to zero.
+        threshold_quantile (float in [0,1]): voxels with intensity below this quantile will be set to zero.
+            Applied only if threshold_absolute is None.
         decimation_params (dict, optional): decimation parameters.
             reduction_rate : expected % decimation reduction rate
             max_clearance : maximum clearance of the decimation
@@ -314,8 +345,8 @@ def bucket_to_mesh(
     if not isinstance(bucket, np.ndarray):
         bucket = bucket_aims_to_ndarray(bucket)
 
-    x,y,z=bucket.T
-    translation = (x.min(),y.min(),z.min())
+    x, y, z = bucket.T
+    translation = (x.min(), y.min(), z.min())
 
     volume = bucket_numpy_to_volume_numpy(bucket)
 
@@ -323,7 +354,7 @@ def bucket_to_mesh(
         log.warn("Thresholding is automatically disabled with smoothing FWHM=0. To remove this message set threshold_quantile=0")
         threshold_quantile = 0
 
-    return volume_to_mesh(volume, gaussian_blur_FWWM, threshold_quantile, translation, decimation_params, smoothing_params)
+    return volume_to_mesh(volume, gaussian_blur_FWWM, threshold_absolute, threshold_quantile, translation, decimation_params, smoothing_params)
 
 # THE OLD IMPLEMENTATION WITH COMMAND LINE TOOLS
 # def build_mesh(volume,
@@ -351,7 +382,7 @@ def bucket_to_mesh(
 #     return aims.read("tmp/combined.mesh")
 
 #     tempfile.mkdtemp()
-    
+
 #     v = volume
 #     # normalize and transform to int16
 #     v = ((v - v.min())/(v.max()-v.min())*256).astype(np.float)
@@ -368,6 +399,7 @@ def bucket_to_mesh(
 #     sh(zcatCmd)
 
 #     return aims.read("tmp/combined.mesh")
+
 
 class PyMesh:
     def __init__(self, aims_mesh=None):
