561b4e600d2417d98a11c6e4069337909c921da0
Marco Pascucci <marpas.paris@gmail.com>
2022-02-08 11:52:20 +0100

    upgrade version micro

--- colorado/info.py
+++ colorado/info.py
@@ -2,7 +2,7 @@
 
 version_major = 0
 version_minor = 2
-version_micro = 0
+version_micro = 1
 version_extra = ''
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
@@ -40,4 +40,3 @@ EXTRA_REQUIRES = {
     "doc": ["sphinx>=" + SPHINX_MIN_VERSION]}
 
 brainvisa_build_model = 'pure_python'
-
